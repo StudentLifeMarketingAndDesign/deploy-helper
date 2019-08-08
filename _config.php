7aeed878e11a365938ae8f0ac5c97a195e974b3e
Dustin Quam <quamsta@gmail.com>
2019-08-08 14:10:18 -0500

    fixed use statement

--- _config.php
+++ _config.php
@@ -1,5 +1,7 @@
 <?php
 
+use SilverStripe\Core\Environment;
+
 if(isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] == '2620:0:e50:6802::27' && $_SERVER['HTTP_X_DEPLOYDEVTOKEN'] == Environment::getEnv('SS_DEPLOYDEVTOKEN')) {
     Environment::setEnv('SS_ENVIRONMENT_TYPE', 'dev');
 }
