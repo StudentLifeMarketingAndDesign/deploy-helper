03aa7fe215680cdb372c27d84c7bca3ba132a244
Dustin Quam <quamsta@gmail.com>
2019-08-08 14:29:05 -0500

    update

--- _config.php
+++ _config.php
@@ -2,6 +2,7 @@
 
 use SilverStripe\Core\Environment;
 
-if(isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] == '2620:0:e50:6802::27' && $_SERVER['HTTP_X_DEPLOYDEVTOKEN'] == Environment::getEnv('SS_DEPLOYDEVTOKEN')) {
+// if(isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] == '2620:0:e50:6802::27' && $_SERVER['HTTP_X_DEPLOYDEVTOKEN'] == Environment::getEnv('SS_DEPLOYDEVTOKEN')) {
+if($_SERVER['HTTP_X_DEPLOYDEVTOKEN'] == Environment::getEnv('SS_DEPLOYDEVTOKEN')) {
     Environment::setEnv('SS_ENVIRONMENT_TYPE', 'dev');
 }
